8a545e10c2b2a0a3d374e4c3e92e9f7de614a3d6
Tatvam <td14@iitbbs.ac.in>
2018-12-08 23:34:14 +0530

    --

--- Part 1 - Data Preprocessing/Missing_Categorical_Data.py
+++ Part 1 - Data Preprocessing/Missing_Categorical_Data.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  8 15:15:29 2018
-
-@author: tatvam
-importing the libraries
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# import the dataset
-
-dataset = pd.read_csv("Data.csv")
-X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
-
 # Take care of missing data
 
 from sklearn.preprocessing import Imputer
@@ -31,3 +13,6 @@ labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
