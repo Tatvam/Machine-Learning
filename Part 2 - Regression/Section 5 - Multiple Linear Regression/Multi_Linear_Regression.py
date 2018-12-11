18d0cfe3f774107300b4d77d74a1e6de28af14f2
Tatvam <td14@iitbbs.ac.in>
2018-12-12 00:48:45 +0530

    --

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multi_Linear_Regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multi_Linear_Regression.py
@@ -72,7 +72,20 @@ regressor_BE.fit(X_train_B, Y_train)
 
 y_pred_BE = regressor_BE.predict(X_test_B)
 
-    
-    
-
-#pd.read_html(Z, header=0, index_col=0)
+"""import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+"""
